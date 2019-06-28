bcbe199a38017586426beaf23652ad194fc407cb
Kevin Florio <kevinf@3dinfotech.com>
2019-06-28 10:53:16 -0700

    Updated

--- WpfTreeView/MainWindow.xaml.cs
+++ WpfTreeView/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -20,9 +22,87 @@ namespace WpfTreeView
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region On Loaded
+        /// <summary>
+        /// When the application first opens
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Get every logical drive on the machine
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                // Create a new item for it
+                var item = new TreeViewItem()
+                {
+                    // Set the header and path
+                    Header = drive,
+                    // and teh full path
+                    Tag = drive
+                };
+
+                // Add dummy item
+                item.Items.Add(null);
+
+                // Listen out for item being expanded
+                item.Expanded += Folder_Expanded;
+
+                // Add it to the main tree-view
+                FolderView.Items.Add(item);
+
+            }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+
+            // If the item only contains the dummy data
+            if (item.Items.Count != 1 || item.Items[0] != null)
+                return;
+
+            // Clear dummy data
+            item.Items.Clear();
+
+            // Get full path
+            var fullPath = (string)item.Tag;
+
+            // Create blank list for directories
+            var directories = new List<string>();
+
+            // Try and get directories
+            // ignoring any issues doing so
+            try
+            {
+                var dirs = Directory.GetDirectories(fullPath);
+                if (dirs.Length > 0)
+                    directories.AddRange(dirs);
+
+            }
+            catch { }
+
+            // For each directory...
+            directories.ForEach(directoryPath =>
+            {
+                var subitem = new TreeViewItem();
+            });
+
+        }
+
+        #endregion
+
+
+
     }
 }
